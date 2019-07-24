9be5c30f9f89c76fdbb81cb128b5e140cbbda886
Samia Bouzid <samia@Samias-MacBook-Pro.local>
2019-07-24 17:47:49 -0400

    updated path to metadata.txt

--- processNewSounds.py
+++ processNewSounds.py
@@ -9,9 +9,6 @@ import re
 #Rename track files
 def renameTracks(origFolder):
 
-    folder = re.split("\W", origFolder)[0] # Pull the CD number out of the folder name
-    lib = re.split(" - ", origFolder)[-1] # Pull the library name out of the folder name
-
     tracks = sorted(os.listdir("."))
     for track in tracks:
         if track.lower().endswith(".txt") or track.lower().endswith(".pdf"):
@@ -34,12 +31,12 @@ def renameTracks(origFolder):
             trackDesc = " ".join(trackDesc)
 
             # Add description to a separate metadata file
-            f.write(trackName + "\t" + trackDesc + "\t" + lib + "\n")
+            f.write(trackName + "\t" + trackDesc + "\t" + folder + "\n")
 
             # Rename the track
-            newPath = "../../RLMG-Local/" # Enter the path relative to the folder where the new files are
+            newPath = "../../../RLMG-Local/" # Enter the path relative to the folder where the new files are
             #print(track, newPath+trackName)
-            os.rename(track, newPath+trackName)
+            os.rename(track, newPath+"DELETE_"+trackName)
         except:
             print("Skipped "+track)
 
@@ -50,12 +47,13 @@ for item in dirContents:
     if os.path.isdir(item):
         folderList.append(item)
 
-f = open("../metadata.txt","a")
+f = open("../../metadata.txt","a")
 
 for folder in folderList:
     mainDir = os.getcwd()
     os.chdir(os.path.join(mainDir,folder))
     renameTracks(folder)
     os.chdir(mainDir)
+    os.rmdir(folder)
 
 f.close()
